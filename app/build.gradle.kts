96a649447b3e5d6ce687caa1afca730f1ad5f2f4
Antonio de la Torre <delatorreperezantonio@gmail.com>
2023-09-18 16:59:40 +0200

    Test de UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.ajtp.horoscapp.CustomTestRunner"
     }
 
     buildTypes {
@@ -92,7 +92,15 @@ dependencies {
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
     testImplementation("io.mockk:mockk:1.12.3")
 
+
+
     //Solo en AndroidTest
+    //UITesting
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-intents:3.4.0")
+    androidTestImplementation ("androidx.fragment:fragment-testing:1.6.1")
+    androidTestImplementation ("com.google.dagger:hilt-android-testing:$daggerVersion")
+    kaptAndroidTest("com.google.dagger:hilt-android-compiler:$daggerVersion")
 }
